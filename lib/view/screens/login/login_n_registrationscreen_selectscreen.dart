d5befb629eef8a724d46300447d73a964c0796ea
mosfeqanik <mosfeqanik01@gmail.com>
2021-08-09 23:27:45 +0600

    faded animation login and registration selection screen  done

--- lib/view/screens/login/login_n_registrationscreen_selectscreen.dart
+++ lib/view/screens/login/login_n_registrationscreen_selectscreen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:wowsell/view/common_widgets/fade_animation.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: HomePage(),
-      )
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
 class HomePage extends StatelessWidget {
@@ -25,74 +24,87 @@ class HomePage extends StatelessWidget {
             children: <Widget>[
               Column(
                 children: <Widget>[
-                  Text("Welcome", style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30
-                  ),),
-                  SizedBox(height: 20,),
-                  Text("Automatic identity verification which enables you to verify your identity",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.grey[700],
-                        fontSize: 15
-                    ),),
+                  FadeAnimation(
+                    1,
+                    Text(
+                      "Welcome",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FadeAnimation(
+                      1.2,
+                      Text(
+                        "Automatic identity verification which enables you to verify your identity",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.grey[700], fontSize: 15),
+                      ))
                 ],
               ),
-              Container(
-                height: MediaQuery.of(context).size.height / 2,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/images/Illustration.png")
-                    )
+              FadeAnimation(
+                1.4,
+                Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/Illustration.png"))),
                 ),
               ),
               Column(
                 children: <Widget>[
-                  MaterialButton(
-                    minWidth: double.infinity,
-                    color: Color.fromRGBO(4, 248, 207,1),
-                    elevation: 0,
-                    height: 60,
-                    onPressed: () {
-                      // Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-                    },
-                    shape: RoundedRectangleBorder(
-                        side: BorderSide(
-                            color: Colors.black
-                        ),
-                        borderRadius: BorderRadius.circular(20)
-                    ),
-                    child: Text("Login", style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18
-                    ),),
-                  ),
-                  SizedBox(height: 20,),
-                   Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border(
-                          bottom: BorderSide(color: Colors.black),
-                          top: BorderSide(color: Colors.black),
-                          left: BorderSide(color: Colors.black),
-                          right: BorderSide(color: Colors.black),
-                        )
-                    ),
-                    child: MaterialButton(
+                  FadeAnimation(
+                    1.6,
+                    MaterialButton(
                       minWidth: double.infinity,
+                      color: Color.fromRGBO(4, 248, 207, 1),
+                      elevation: 0,
                       height: 60,
                       onPressed: () {
-                        // Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
                       },
-                      color: Color.fromRGBO(251,243,100,1),
-                      elevation: 0,
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)
+                          side: BorderSide(color: Colors.black),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 18),
                       ),
-                      child: Text("Sign up", style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18
-                      ),),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FadeAnimation(
+                    1.8,
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border(
+                            bottom: BorderSide(color: Colors.black),
+                            top: BorderSide(color: Colors.black),
+                            left: BorderSide(color: Colors.black),
+                            right: BorderSide(color: Colors.black),
+                          )),
+                      child: MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: () {
+                          // Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
+                        },
+                        color: Color.fromRGBO(251, 243, 100, 1),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text(
+                          "Sign up",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, fontSize: 18),
+                        ),
+                      ),
                     ),
                   )
                 ],
