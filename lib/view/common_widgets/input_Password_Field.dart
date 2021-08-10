cc874420e6d4a585f9a13c005be181e5a50a756b
mosfeqanik <mosfeqanik01@gmail.com>
2021-08-10 22:43:04 +0600

    provider obscure added

--- lib/view/common_widgets/input_Password_Field.dart
+++ lib/view/common_widgets/input_Password_Field.dart
@@ -10,48 +10,55 @@ class InputPasswordField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final providerData = Provider.of<EcommerceProvider>(context);
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          label,
-          style: TextStyle(
-              fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        TextField(
-          obscureText: isObscure,
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey[400])),
-            border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey[400])),
-            suffixIcon: isObscure == true
-                ? IconButton(
-                    onPressed: () {
-                      providerData.isObscure = false;
-                    },
-                    icon: Icon(
-                      Icons.remove_red_eye,
-                      size: 20,
-                    ))
-                : IconButton(
-                    onPressed: () {
-                      providerData.isObscure = true;
-                    },
-                    icon: Icon(
-                      Icons.visibility_off,
-                      size: 20,
-                    )),
-          ),
-        ),
-        SizedBox(height: 30),
-      ],
+
+    return Consumer<EcommerceProvider>(
+        builder: (_, provider, ___) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                label,
+                style: TextStyle(
+                    fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextField(
+                obscureText: provider.isObscure,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey[400])),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey[400])),
+                  suffixIcon: provider.isObscure == true
+                      ? IconButton(
+                      onPressed: () {
+                        final providerData = Provider.of<EcommerceProvider>(context,listen:false);
+                        providerData.isObscure = false;
+                      },
+                      icon: Icon(
+                        Icons.remove_red_eye,
+                        size: 20,
+                      ))
+                      : IconButton(
+                      onPressed: () {
+                        final providerData = Provider.of<EcommerceProvider>(context,listen:false);
+                        providerData.isObscure = true;
+                      },
+                      icon: Icon(
+                        Icons.visibility_off,
+                        size: 20,
+                      )),
+                ),
+              ),
+              SizedBox(height: 30),
+            ],
+          );
+        }
+      ,
     );
   }
 }
