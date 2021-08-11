deb06a206391df2f11d44193ff93821412655355
mosfeqanik <mosfeqanik01@gmail.com>
2021-08-11 15:06:56 +0600

    navbar added

--- lib/view/screens/mainpage/navbar.dart
+++ lib/view/screens/mainpage/navbar.dart
@@ -1,8 +1,11 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:wowsell/view/screens/mainpage/cart_page.dart';
 import 'package:wowsell/view/screens/mainpage/homepage.dart';
 import 'package:wowsell/view/screens/mainpage/order_page.dart';
+
+void main() => runApp(BottomNavbar());
 
 class BottomNavbar extends StatefulWidget {
   @override
@@ -10,28 +13,46 @@ class BottomNavbar extends StatefulWidget {
 }
 
 class _BottomNavbarState extends State<BottomNavbar> {
+  var _page = 1;
+
   final pages = [
     OrderPage(),
     HomePage(),
     CartPage(),
   ];
-
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: CurvedNavigationBar(
-        index: 0,
-        color: Colors.white,
-        items: [
-          Icon(
-            Icons.bookmark_added,
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          bottomNavigationBar: CurvedNavigationBar(
+            index: 1,
+            color: Colors.white,
+            backgroundColor: Colors.blue,
+            animationCurve: Curves.easeIn,
+            animationDuration: Duration(milliseconds: 600),
+            onTap: (index) {
+              setState(() {
+                _page = index;
+              });
+            },
+            items: [
+              Icon(
+                Icons.bookmark_added,
+                size: 35,
+              ),
+              Icon(
+                Icons.house,
+                size: 35,
+              ),
+              Icon(
+                FontAwesomeIcons.gamepad,
+                size: 35,
+              ),
+            ],
           ),
-          Icon(Icons.house),
-          Icon(
-            Icons.shopping_basket_outlined,
-          ),
-        ],
-      ),
-    );
+          body: pages[_page],
+        ));
   }
 }
